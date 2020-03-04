6f2af58e035af69d39a848b2a14424b69174261a
pangfumin <fumin.pang@ninebot.com>
2020-03-04 16:31:27 +0800

    make it run no ros

---
+++ run_vio.sh
@@ -0,0 +1,2 @@
+
+./build/euroc_VIO Vocabulary/ORBvoc.bin config/euroc.yaml /home/pang/dataset/euroc/MH_01_easy/mav0/cam0/data Examples/VIO/EuRoC_TimeStamps/MH01.txt Examples/VIO/EuRoC_TimeStamps/MH01_IMU.txt
